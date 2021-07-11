9c1c825033436824c1ed06f903f6aed1624e1022
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-12 00:07:52 +0800

    finished chat functionality

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,24 +1,41 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 3 }, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("Connected...");
-  },
+document.addEventListener('turbolinks:load', () => {
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    const room_element = document.getElementById('room-id');
+    const room_id = Number(room_element.getAttribute('data-room-id'));
+    const send_button = document.getElementById('send-btn');
+    const input_box = document.getElementById('message_content');
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    const element = document.getElementById('user-id');
-    const user_id = Number(element.getAttribute('data-user-id'));
-   
-    let html;
-    user_id === data.message.user_id ? html = data.own_message : html = data.not_own_message;
+    // for terminating other subscriptions when connected to a new subscription
+    console.log(consumer.subscriptions);
+    consumer.subscriptions.subscriptions.forEach(subs => {
+        consumer.subscriptions.remove(subs);
+    });
 
-    const messageContainer = document.getElementById('messages');
-    messageContainer.innerHTML += html;
-  }
+    consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
+        connected() {
+          // Called when the subscription is ready for use on the server
+          console.log("Connected to " + room_id);
+        },
+      
+        disconnected() {
+          // Called when the subscription has been terminated by the server
+        },
+      
+        received(data) {
+          // Called when there's incoming data on the websocket for this channel
+          const user_element = document.getElementById('user-id');
+          const user_id = Number(user_element.getAttribute('data-user-id'));
+         
+          let html;
+          user_id === data.message.user_id ? html = data.own_message : html = data.not_own_message;
+      
+          const messageContainer = document.getElementById('messages');
+          messageContainer.innerHTML += html;
+          send_button.disabled = false;
+          input_box.value = '';
+        }
+      });
 });
+
